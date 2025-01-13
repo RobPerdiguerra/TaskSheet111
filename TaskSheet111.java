baf3ead854d9d87aa5471528b427b53f0eec5a86
RobPerdiguerra <perdiguerrarobinlouis@gmail.com>
2025-01-13 00:53:42 +0000

    Done TaskSheet111

--- TaskSheet111.java
+++ TaskSheet111.java
@@ -1,1 +1,7 @@
-
+public class TaskSheet111 {
+    static class HelloWorldApp {
+        public static void main(String[] args) {
+            System.out.println("Kamusta Mundo!");
+        }
+    }
+}
